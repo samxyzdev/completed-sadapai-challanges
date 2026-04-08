4cdd8413f6e08124c060b5943efcd0bd4dc86597
Sameer Ahmed <samxyzdev@gmail.com>
2026-04-08 20:23:34 +0530

    responsig navbar and stepper completed

--- 9-stepper/src/components/Stepper.tsx
+++ 9-stepper/src/components/Stepper.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const information = [
   {
     title: "Contact Details",
@@ -18,27 +20,107 @@ const information = [
 ];
 
 export function Stepper() {
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const handleNext = () => {
+    if (currentStep < information.length) {
+      setCurrentStep((prev) => prev + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentStep > 0) {
+      setCurrentStep((prev) => prev - 1);
+    }
+  };
+
+  const isFullyCompleted = currentStep === information.length;
+
   return (
-    <div className="flex justify-center items-center pt-8">
-      {information.map(({ title }, index) => (
-        <RoundCircle key={index} title={title} index={index} />
-      ))}
+    <div>
+      <div className="flex justify-center items-center pt-8">
+        {information.map(({ title }, index) => (
+          <RoundCircleWithLine
+            key={index}
+            index={index}
+            title={title}
+            isLast={index + 1 === information.length}
+            isActive={index === currentStep}
+            isCompleted={index < currentStep}
+          />
+        ))}
+      </div>
+      {isFullyCompleted ? (
+        <StepperDescription description="Order Delivered successfully 🎉" />
+      ) : (
+        <StepperDescription
+          description={information[currentStep].description}
+        />
+      )}
+
+      <div className="flex justify-center items-center gap-24">
+        <button
+          disabled={currentStep === 0}
+          className="px-4 py-2 mt-4 bg-[#CCCCCC] rounded-md text-xl
+          disabled:bg-gray-300 disabled:text-gray-400 disabled:cursor-not-allowed"
+          onClick={handlePrev}
+        >
+          Previous
+        </button>
+        <button
+          disabled={isFullyCompleted}
+          onClick={handleNext}
+          className="px-4 py-2 mt-4 bg-[#CCCCCC] rounded-md text-xl disabled:bg-gray-300 disabled:text-gray-400 disabled:cursor-not-allowed "
+        >
+          {currentStep >= information.length - 1 ? "Finish" : "Next"}
+        </button>
+      </div>
     </div>
   );
 }
 
-function RoundCircle({ title, index }) {
+function RoundCircleWithLine({
+  index,
+  title,
+  isLast,
+  isActive,
+  isCompleted,
+}: {
+  index: number;
+  title: string;
+  isLast: boolean;
+  isActive: boolean;
+  isCompleted: boolean;
+}) {
+  let circleBgClass = "bg-[#CCCCCC] text-black";
+  if (isActive) circleBgClass = "bg-[#4746F7] text-white";
+  if (isCompleted) circleBgClass = "bg-[#1EC41F] text-white";
+
   return (
-    <div className="relative flex flex-col items-center">
-      {!isLast && (
-        <div className="absolute top-4 left-1/2 w-full h-[2px] bg-gray-300 -z-10 "></div>
-      )}
-      <div className="w-8 h-8 rounded-full bg-[#CCCCCC] border-2 flex justify-center items-center font-medium">
-        {index + 1}
+    <div>
+      <div className="flex items-center ">
+        <div
+          className={`relative border-2 border-black w-8 h-8 shrink-0 rounded-full flex justify-center items-center ${circleBgClass}  z-10`}
+        >
+          {isCompleted ? "✓" : index + 1}
+          <p className="absolute top-10 left-1/2 -translate-x-1/2 text-center text-wrap text-md text-black md:text-nowrap  ">
+            {title}
+          </p>
+        </div>
+        {!isLast && (
+          <div
+            className={`w-16 md:w-32 lg:w-48 h-1 ${isCompleted ? "bg-[#008000]" : "bg-[#808080]"} `}
+          />
+        )}
       </div>
-      <p className="mt-2 font-medium text-sm text-gray-700 text-center">
-        {title}
-      </p>
+    </div>
+  );
+}
+
+function StepperDescription({ description }: { description: string }) {
+  return (
+    <div>
+      <h2 className="text-center mt-14 text-xl font-semibold">{description}</h2>
     </div>
   );
 }
